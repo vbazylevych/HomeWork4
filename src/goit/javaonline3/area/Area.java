9872f86beb8c12dd9c6e55577b4ff4d3be9edad6
vbazylevych <valeriyabazilevich@gmail.com>
2016-06-01 23:05:14 +0300

    1

---
+++ src/goit/javaonline3/area/Area.java
@@ -0,0 +1,11 @@
+package goit.javaonline3.area;
+
+/**
+ * Created by Ler4enko on 01.06.2016.
+ */
+public class Area {
+
+    public double countTriangleArea(double side, double height){
+        return (0.5 * side * visota);
+    }
+}
