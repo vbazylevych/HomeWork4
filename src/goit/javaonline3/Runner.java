c4e7dd152d8e0bd5e7e3463e7df1ddb7655cdb64
vbazylevych <vbazylevych@playtika.com>
2016-06-14 15:52:07 +0300

    add check of negative side and height edit grammar mistake done all methods static

--- src/goit/javaonline3/Runner.java
+++ src/goit/javaonline3/Runner.java
@@ -23,12 +23,8 @@ class Runner {
             System.out.println("Enter height:");
             double height = input.nextDouble();
 
-            if(side >= 0 && height >= 0){
-                System.out.println("Triangle area with side = " + side + " and height = "+ height + " is "
-                        + String.format("2%.2f", Area.countTriangleArea(side, height)));
-            } else {
-                System.out.println("You entered incorect side or height.Triangle area won't be counted!");
-            }
+            System.out.println("Triangle area with side = " + side + " and height = "+ height + " is "
+                    + String.format("2%.2f", Area.countTriangleArea(side, height)));
         } catch (InputMismatchException e) {
             System.out.println("You entered incorrect side or height. Triangle area won't be counted!");
         }
@@ -38,7 +34,7 @@ class Runner {
         System.out.println("100500 degrees from Celsius to Fahrenheit is "
                 + String.format("%.2f", Temperature.fromCelsiusToFahrenheit(100500)));
         System.out.println("100500 degrees from Fahrenheit to Celsius is " + String.format("%.2f", Temperature.fromFahrenheitToCelsius(100500)));
-        System.out.println("Distance beatween (0;0) and (0;5) = " + String.format("%.2f", Distance.countDistance(0, 0, 0, 5)));
+        System.out.println("Distance between (0;0) and (0;5) = " + String.format("%.2f", Distance.countDistance(0, 0, 0, 5)));
     }
 }
 
--- src/goit/javaonline3/area/Area.java
+++ src/goit/javaonline3/area/Area.java
@@ -1,20 +1,24 @@
 package goit.javaonline3.area;
 
+import java.util.InputMismatchException;
+
 /**
  * Created by Ler4enko on 01.06.2016.
  */
 public class Area {
 
-    public static double countTriangleArea(double side, double height) {
+    public static double countTriangleArea (double side, double height) {
+        if(side <= 0 || height <= 0) {
+            throw new InputMismatchException();
+        }
         return (0.5 * side * height);
     }
 
-    public static double countSquareArea(double side, double height){
+    public static double countSquareArea(double side, double height) {
         return (side * height);
     }
 
-    public static double countCircleArea(double radius)
-    {
+    public static double countCircleArea(double radius) {
         return (Math.PI * Math.pow(radius, 2));
     }
 }
